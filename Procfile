c5c69b662a944a33d7c8972fa822fff9c3185a0a
n3bukadn3zar <n3bukadn3zar@n3bukadn3zars-MacBook-Air.local>
2014-10-26 17:08:02 +0100

    Initital commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath="$PWD/reservo" wsgi:application
---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import reservo
+version = reservo.__version__
+
+setup(
+    name='reservo',
+    version=version,
+    author='',
+    author_email='ro@arachia.com',
+    packages=[
+        'reservo',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.6.5',
+    ],
+    zip_safe=False,
+    scripts=['reservo/manage.py'],
+)
