7c2eefc44c48939fffb9bd199b8f32b009281ea1
n3bukadn3zar <n3bukadn3zar@n3bukadn3zars-MacBook-Air.local>
2014-10-26 22:13:42 +0100

    Material Design added

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'Django>=1.6.5',
+        'Django>=1.7',
     ],
     zip_safe=False,
     scripts=['reservo/manage.py'],
